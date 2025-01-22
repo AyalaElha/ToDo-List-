b52e5479c6abc46cf06bf48f7c2bc67d555bf5f6
AyalaElha <a0556768626@gmail.com>
2025-01-23 01:42:20 +0200

    Add React project as part of main project

---
+++ Client/ToDoListReact/src/service.js
@@ -0,0 +1,40 @@
+import axios from 'axios';
+
+axios.defaults.baseURL="http://localhost:5248"
+
+axios.interceptors.response.use(
+  (response) => {
+    // אם התגובה תקינה, פשוט מחזירים אותה
+    return response;
+  },
+  (error) => {
+    // אם התגובה מכילה שגיאה, נרשום אותה ללוג
+    console.error('Error:', error.response ? error.response.data : error.message);
+    return Promise.reject(error);
+  }
+);
+
+export default {
+  getTasks: async () => {
+    const result = await axios.get(`/tasks`)    
+    console.log('get all tasks');
+    return result.data;
+  },
+
+  addTask: async(name)=>{
+    const result = await axios.post(`/tasks`,{ name:name , isComplete: false })
+    console.log('added task ');
+    return result.data;
+  },
+
+  setCompleted: async(id, isComplete)=>{
+    const result = await axios.put(`/tasks/${id}`,{isComplete:isComplete})
+    console.log('setCompleted', {id, isComplete})
+    return result.data;
+  },
+
+  deleteTask:async(id)=>{
+    const result = await axios.delete(`/tasks/${id}`)
+    console.log('deleteTask')
+  }
+};
